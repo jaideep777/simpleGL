2b9d344c47e1b262be9abea77ebeb39fa579ee08
chethana <jaideep777@gmail.com>
2018-10-05 23:34:07 +0530

    working: display data from PLY file

--- src/main.cpp
+++ src/main.cpp
@@ -10,58 +10,112 @@ using namespace std;
 
 int main(int argc, char **argv){
 
-	float pos[] = { 50, 50, -10,//0.5,
-				  25, 50, -20,//-0.5,
-				  50,  25, -30,//  -1,
-				  25,   25, 0 //  1,
-				};
+	int nverts;
+	float *pos9;
+	float4 *col9;
+	
+	ifstream fin("odm_georeferenced_model.ply");
+	string s, u;
+	int prop_count = 0;
+	while (fin >> s && s != "end_header"){
+		if (s == "element"){
+			fin >> u;
+			if (u == "vertex") fin >> nverts;
+		}
+		if (s == "property") ++prop_count;
+	}
 
-	float4 col[] = {make_float4(1,1,1, 1),
-				    make_float4(1,0,0, 1),
-				    make_float4(0,1,0, 1),
-				    make_float4(0,0,1, 1)
-				   };
+	cout << "PLY: vertices = " << nverts << ", properties = " << prop_count << endl; 	
+
+	pos9 = new float[3*nverts];
+	col9 = new float4[nverts];
+	
+	float * vals = new float[prop_count];
+	
+	for (int i=0; i<nverts; ++i){
+		for (int k=0; k<prop_count; ++k){
+			fin >> vals[k];
+		}
+		pos9[3*i+0] = vals[1];
+		pos9[3*i+1] = vals[2];
+		pos9[3*i+2] = vals[0];
+		
+		col9[i] = make_float4(vals[3]/255, vals[4]/255, vals[5]/255, 1);
+	} 
+	
+
+	for (int i=0; i<5; ++i){
+		cout << 
+		pos9[3*i+0] << " " << 
+		pos9[3*i+1] << " " << 
+		pos9[3*i+2] << " " <<
+		col9[i].x << " " << col9[i].y << " " << col9[i].z << " " << col9[i].w << endl;  
+	} 
+
 
 	init_hyperGL(&argc, argv);
-	
-//	int nx = 100, ny = 100;  
-//	float L = 2;
-//	ColorMap res_shape("res", 100, nx, 0, L);
-//	float2 cmap_pos[res_shape.nVertices];
-//	res_shape.createGridCentres(cmap_pos); 
-//	res_shape.createShaders();
-//	res_shape.createVBO(cmap_pos, res_shape.nVertices*sizeof(float2));	
-//	res_shape.createColorBuffer();
-//	vector <float> res(nx*ny); for (int i=0; i<nx*ny; ++i) res[i]=i; 
-//	res_shape.updateColors(&res[0], nx*ny);
 
 
-	Shape pt(4, 3, "points"); //, 4, -1, 1);
+	Shape pt(nverts, 3, "points"); //, 4, -1, 1);
 //	pt.createShaders();
-	pt.pointSize = 4;
-	pt.setVertices(pos);	
+	pt.pointSize = 1;
+	pt.setVertices(pos9);	
 //	pt.createColorBuffer();
-	pt.setColors(col);
-	pt.autoExtent(pos);
-//	pt.setDefaultColor();
-	
+	pt.setColors(col9);
+	pt.autoExtent(pos9);
 
-	float pos2[] = { -.25, -.25, //0,//0.5,
-				  -.25, .25, //0,//-0.5,
-				  .25,  -.25, //0,//  -1,
-				  .25,   .25 //0 //  1,
-					};
 
-	float4 col2[] = {make_float4(1,1,0, 0.5),
-				    make_float4(1,0,1, 0.5),
-				    make_float4(0,1,0, 0.5),
-				    make_float4(1,1,1, 0.5)
-				   };
-					
-	Shape2D pt2(4, "triangles");
-	pt2.setVertices(pos2);
-	pt2.setColors(col2);
-	pt2.setExtent(-.5, .5, -.5, .5);
+//	float pos[] = { 50, 50, -10,//0.5,
+//				  25, 50, -20,//-0.5,
+//				  50,  25, -30,//  -1,
+//				  25,   25, 0 //  1,
+//				};
+
+//	float4 col[] = {make_float4(1,1,1, 1),
+//				    make_float4(1,0,0, 1),
+//				    make_float4(0,1,0, 1),
+//				    make_float4(0,0,1, 1)
+//				   };
+
+//	
+////	int nx = 100, ny = 100;  
+////	float L = 2;
+////	ColorMap res_shape("res", 100, nx, 0, L);
+////	float2 cmap_pos[res_shape.nVertices];
+////	res_shape.createGridCentres(cmap_pos); 
+////	res_shape.createShaders();
+////	res_shape.createVBO(cmap_pos, res_shape.nVertices*sizeof(float2));	
+////	res_shape.createColorBuffer();
+////	vector <float> res(nx*ny); for (int i=0; i<nx*ny; ++i) res[i]=i; 
+////	res_shape.updateColors(&res[0], nx*ny);
+
+
+//	Shape pt(4, 3, "points"); //, 4, -1, 1);
+////	pt.createShaders();
+//	pt.pointSize = 4;
+//	pt.setVertices(pos);	
+////	pt.createColorBuffer();
+//	pt.setColors(col);
+//	pt.autoExtent(pos);
+////	pt.setDefaultColor();
+//	
+
+//	float pos2[] = { -.25, -.25, //0,//0.5,
+//				  -.25, .25, //0,//-0.5,
+//				  .25,  -.25, //0,//  -1,
+//				  .25,   .25 //0 //  1,
+//					};
+
+//	float4 col2[] = {make_float4(1,1,0, 0.5),
+//				    make_float4(1,0,1, 0.5),
+//				    make_float4(0,1,0, 0.5),
+//				    make_float4(1,1,1, 0.5)
+//				   };
+//					
+//	Shape2D pt2(4, "triangles");
+//	pt2.setVertices(pos2);
+//	pt2.setColors(col2);
+//	pt2.setExtent(-.5, .5, -.5, .5);
 	
 	float pos3[] = {-100,0,0, 100,0,0, 0,-100,0, 0,100,0, 0,0,-100, 0,0,100};
 	float4 col3[] = {make_float4(1,0,0, 0.5),
@@ -76,14 +130,15 @@ int main(int argc, char **argv){
 	axis.setVertices(pos3);
 	axis.setColors(col3);
 
-	Palette p(20);
-	p.create_grayscale();
-	p.print();
+//	Palette p(20);
+//	p.create_grayscale();
+//	p.print();
+//	
+//	float v[] = {0.1, 0.2, 0.3, 0.4, 0.1, 0.2, 0.3, 0.4};
+//	vector <glm::vec4> c = p.map_values(v, 8);
+//	for (int i=0; i<8; ++i) cout << c[i].r << " " << c[i].g << " " << c[i].b << " " << c[i].a << "\n";
+//	cout << endl; 
 	
-	float v[] = {0.1, 0.2, 0.3, 0.4, 0.1, 0.2, 0.3, 0.4};
-	vector <glm::vec4> c = p.map_values(v, 8);
-	for (int i=0; i<8; ++i) cout << c[i].r << " " << c[i].g << " " << c[i].b << " " << c[i].a << "\n";
-	cout << endl; 
 	
 	while(1){       // infinite loop needed to poll anim_on signal.
 		glutMainLoopEvent();
